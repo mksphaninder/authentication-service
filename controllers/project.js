a9f1bdc8f9e7d4670dde851dc4026f5c7876c19c
mksphaninder <julienbi@unity3d.com>
2020-08-02 20:56:08 -0400

    - Added all the CRUD functionality for the projects

--- controllers/project.js
+++ controllers/project.js
@@ -1,30 +1,127 @@
-const axios = require('axios');
+const axios = require("axios");
 
 exports.showProjects = (req, res, next) => {
-    axios.get(`http://localhost:8080/users/${req.params.userId}/projects`)
-    .then(response => {
-        // console.log(response);
-        res.statusCode = response.status;
-        res.json(response.data);
+  axios
+    .get(`http://localhost:8080/users/${req.params.userId}/projects`)
+    .then((response) => {
+      // console.log(response);
+      res.statusCode = response.status;
+      res.json(response.data);
     })
-    .catch(err => {
-        res.status = 500;
-        res.json({
-            message: "Internal server error"
-        })
+    .catch((err) => {
+      res.status = 500;
+      res.json({
+        message: "Internal server error",
+      });
     });
 };
 
 exports.addProjects = (req, res, next) => {
-    axios.post(`http://localhost:8080/users/${req.params.userId}/projects`, req.body)
-    .then(response => {
-        console.log(response);
-        res.statusCode = response.status;
-        res.json(response.data);
+  axios
+    .post(`http://localhost:8080/users/${req.params.userId}/projects`, req.body)
+    .then((response) => {
+      console.log(response);
+      res.statusCode = response.status;
+      res.json(response.data);
     })
-    .catch(err => {
-        console.log(err);
-        res.statusCode = err.response.status;
-        res.json(err.response.data);
+    .catch((err) => {
+      console.log(err);
+      res.statusCode = err.response.status;
+      res.json(err.response.data);
     });
 };
+
+exports.editProject = (req, res, next) => {
+  axios
+    .put(
+      `http://localhost:8080/users/${req.params.userId}/projects/${req.params.projectId}`,
+      req.body
+    )
+    .then((response) => {
+      res.statusCode = response.status;
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.statusCode = err.response.status;
+      res.json(err.response.data);
+    });
+};
+
+exports.removeProject = (req, res, next) => {
+    axios
+    .delete(
+      `http://localhost:8080/users/${req.params.userId}/projects/${req.params.projectId}`
+    )
+    .then((response) => {
+      res.statusCode = response.status;
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.statusCode = err.response.status;
+      res.json(err.response.data);
+    });
+};
+
+exports.showTasks = (req, res, next) => {
+    axios
+    .get(
+      `http://localhost:8080/users/${req.params.userId}/projects/${req.params.projectId}/tasks`
+    )
+    .then((response) => {
+      res.statusCode = response.status;
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log(err.data);
+      res.statusCode = err.response.status;
+      res.json(err.response.data);
+    });
+}
+
+exports.addTasks = (req, res, next) => {
+    axios
+    .post(
+      `http://localhost:8080/users/${req.params.userId}/projects/${req.params.projectId}/tasks`, req.body
+    )
+    .then((response) => {
+      res.statusCode = response.status;
+      res.json(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.statusCode = err.response.status;
+      res.json(err.response.data);
+    });
+}
+
+exports.editTasks = (req, res, next) => {
+  axios
+  .put(
+    `http://localhost:8080/users/${req.params.userId}/projects/${req.params.projectId}/tasks/${req.params.taskId}`, req.body
+  )
+  .then((response) => {
+    res.statusCode = response.status;
+    res.json(response.data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.statusCode = err.response.status;
+    res.json(err.response.data);
+  });
+}
+
+exports.removeTasks = (req, res, next) => {
+  axios
+  .delete(`http://localhost:8080/users/${req.params.userId}/projects/${req.params.projectId}/tasks/${req.params.taskId}`)
+  .then((response) => {
+    res.statusCode = response.status;
+    res.json(response.data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.statusCode = err.response.status;
+    res.json(err.response.data);
+  });
+}
--- routes/project.js
+++ routes/project.js
@@ -4,8 +4,20 @@ const router = express.Router();
 const isAuth = require('../middleware/is-auth.js');
 const projectController = require('../controllers/project.js');
 
-router.get('/users/:userId/projects', isAuth, projectController.showProjects)
+router.get('/users/:userId/projects', isAuth, projectController.showProjects);
 
-router.post('/users/:userId/projects', isAuth, projectController.addProjects)
+router.post('/users/:userId/projects', isAuth, projectController.addProjects);
+
+router.put('/users/:userId/projects/:projectId', isAuth, projectController.editProject);
+
+router.delete('/users/:userId/projects/:projectId', isAuth, projectController.removeProject);
+
+router.get('/users/:userId/projects/:projectId/tasks', isAuth, projectController.showTasks);
+
+router.post('/users/:userId/projects/:projectId/tasks', isAuth, projectController.addTasks);
+
+router.put('/users/:userId/projects/:projectId/tasks/:taskId', isAuth, projectController.editTasks);
+
+router.delete('/users/:userId/projects/:projectId/tasks/:taskId', isAuth, projectController.removeTasks);
 
 module.exports = router;
